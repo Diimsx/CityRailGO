a2860142b63cb891a1ab56ce7eb6a3bd272b142b
Diimsx <dimasml476@gmail.com>
2026-06-04 10:03:26 +0700

    Refactor Rute class for improved readability

--- src/cityrailgo/model/Rute.java
+++ src/cityrailgo/model/Rute.java
@@ -9,30 +9,55 @@ public class Rute {
     public Rute() {}
 
     public Rute(int id, String asal, String tujuan, double jarak) {
-        this.id = id;
-        this.asal = asal;
+        this.id     = id;
+        this.asal   = asal;
         this.tujuan = tujuan;
-        this.jarak = jarak;
+        this.jarak  = jarak;
     }
 
     public String getInfoRute() {
-        return asal + " -> " + tujuan + " (" + jarak + " km)";
+        return asal + " → " + tujuan + " (" + jarak + " km)";
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() {
+        return id;
+    }
 
-    public String getAsal() { return asal; }
-    public void setAsal(String asal) { this.asal = asal; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public String getTujuan() { return tujuan; }
-    public void setTujuan(String tujuan) { this.tujuan = tujuan; }
+    public String getAsal() {
+        return asal;
+    }
 
-    public double getJarak() { return jarak; }
-    public void setJarak(double jarak) { this.jarak = jarak; }
+    public void setAsal(String asal) {
+        this.asal = asal;
+    }
+
+    public String getTujuan() {
+        return tujuan;
+    }
+
+    public void setTujuan(String tujuan) {
+        this.tujuan = tujuan;
+    }
+
+    public double getJarak() {
+        return jarak;
+    }
+
+    public void setJarak(double jarak) {
+        this.jarak = jarak;
+    }
 
     @Override
     public String toString() {
-        return "Rute{" + "id=" + id + ", rute='" + getInfoRute() + '\'' + '}';
+        return "Rute{" +
+                "id=" + id +
+                ", asal='" + asal + '\'' +
+                ", tujuan='" + tujuan + '\'' +
+                ", jarak=" + jarak + " km" +
+                '}';
     }
 }
