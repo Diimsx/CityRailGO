6b82b670386cdd0e8ab22c93d7a3a28f029aa1d2
rayssaameyliananda23-collab <rayssaameyliananda23@gmail.com>
2026-06-12 16:02:05 +0700

    Update Admin.java

--- src/cityrailgo/model/Admin.java
+++ src/cityrailgo/model/Admin.java
@@ -1,77 +1,47 @@
 package cityrailgo.model;
- 
+
+import java.time.LocalDate;
+
 public class Admin extends User {
-    private String nama;
- 
-    public Admin() {
-        super();
+
+    private String levelAkses;
+    private String jenisKelamin;
+    private LocalDate tglLahir;
+
+    public Admin(String username,
+                 String password,
+                 String email,
+                 String namaLengkap,
+                 String noTelepon,
+                 String levelAkses,
+                 String jenisKelamin,
+                 LocalDate tglLahir) {
+
+        super(username,
+                password,
+                email,
+                namaLengkap,
+                noTelepon);
+
+        this.levelAkses = levelAkses;
+        this.jenisKelamin = jenisKelamin;
+        this.tglLahir = tglLahir;
     }
- 
-    public Admin(int id, String username, String email, String password, String nama) {
-        super(id, username, email, password);
-        this.nama = nama;
+
+    public String getLevelAkses() {
+        return levelAkses;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public LocalDate getTglLahir() {
+        return tglLahir;
     }
 
     @Override
     public String getRole() {
-        return "ADMIN";
-    }
- 
-    @Override
-    public String getInfo() {
-        return "Admin: " + nama + " (@" + getUsername() + ")";
-    }
-
-    public void tambahJadwal(Jadwal jadwal) {
-        JadwalDAO dao = new JadwalDAO();
-        dao.tambah(jadwal);
-        System.out.println("[Admin] Jadwal berhasil ditambahkan: " + jadwal);
-    }
- 
-    public void hapusJadwal(int id) {
-        JadwalDAO dao = new JadwalDAO();
-        dao.hapus(id);
-        System.out.println("[Admin] Jadwal dengan ID " + id + " berhasil dihapus.");
-    }
- 
-    public void tambahKereta(Kereta kereta) {
-        KeretaDAO dao = new KeretaDAO();
-        dao.tambah(kereta);
-        System.out.println("[Admin] Kereta berhasil ditambahkan: " + kereta);
-    }
-
-    public void tambahRute(Rute rute) {
-        RuteDAO dao = new RuteDAO();
-        dao.tambah(rute);
-        System.out.println("[Admin] Rute berhasil ditambahkan: " + rute);
-    }
-
-    public void tambahPromo(Promo promo) {
-        PromoDAO dao = new PromoDAO();
-        dao.tambah(promo);
-        System.out.println("[Admin] Promo berhasil ditambahkan: " + promo.getKodePromo());
-    }
-
-    public void nonaktifkanPromo(int id) {
-        PromoDAO dao = new PromoDAO();
-        dao.nonaktifkan(id);
-        System.out.println("[Admin] Promo dengan ID " + id + " telah dinonaktifkan.");
-    }
-
-    public String getNama() {
-        return nama;
-    }
- 
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    @Override
-    public String toString() {
-        return "Admin{" +
-                "id=" + getId() +
-                ", username='" + getUsername() + '\'' +
-                ", nama='" + nama + '\'' +
-                '}';
+        return "Admin";
     }
 }
