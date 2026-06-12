da925001245722bcc3d9a3fd491c4da63ad6dee8
rayssaameyliananda23-collab <rayssaameyliananda23@gmail.com>
2026-06-12 16:21:38 +0700

    Update TiketDAO.java

--- src/cityrailgo/dao/TiketDAO.java
+++ src/cityrailgo/dao/TiketDAO.java
@@ -1,5 +1,57 @@
 package cityrailgo.dao;
 
-public class TiketDAO {
-    
+import cityrailgo.model.Rute;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RuteDAO {
+
+    private List<Rute> daftarRute = new ArrayList<>();
+
+    public Rute findById(int id) {
+
+        for (Rute rute : daftarRute) {
+            if (rute.getId() == id) {
+                return rute;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Rute> findAll() {
+        return daftarRute;
+    }
+
+    public List<Rute> findByAsal(String stasiunAsal) {
+
+        List<Rute> hasil = new ArrayList<>();
+
+        for (Rute rute : daftarRute) {
+            if (rute.getAsal().equalsIgnoreCase(stasiunAsal)) {
+                hasil.add(rute);
+            }
+        }
+
+        return hasil;
+    }
+
+    public boolean save(Rute rute) {
+        return daftarRute.add(rute);
+    }
+
+    public boolean update(Rute rute) {
+        return true;
+    }
+
+    public boolean delete(int id) {
+
+        Rute rute = findById(id);
+
+        if (rute != null) {
+            return daftarRute.remove(rute);
+        }
+
+        return false;
+    }
 }
