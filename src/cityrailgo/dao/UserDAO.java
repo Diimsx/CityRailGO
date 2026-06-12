dc1a3be4bfaea49446ca92217d23523807353b40
rayssaameyliananda23-collab <rayssaameyliananda23@gmail.com>
2026-06-12 16:15:08 +0700

    Update UserDAO.java

--- src/cityrailgo/dao/UserDAO.java
+++ src/cityrailgo/dao/UserDAO.java
@@ -1,5 +1,51 @@
 package cityrailgo.dao;
 
+import cityrailgo.model.User;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDAO {
-    
+
+    private List<User> daftarUser = new ArrayList<>();
+
+    public User findById(int id) {
+
+        for (User user : daftarUser) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public User findByUsername(String username) {
+
+        for (User user : daftarUser) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean save(User user) {
+        return daftarUser.add(user);
+    }
+
+    public boolean update(User user) {
+        return true;
+    }
+
+    public boolean delete(int id) {
+
+        User user = findById(id);
+
+        if (user != null) {
+            return daftarUser.remove(user);
+        }
+
+        return false;
+    }
 }
