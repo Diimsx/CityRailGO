34d8ec94996dec5fc2aec169c3b1f68489903f78
rayssaameyliananda23-collab <rayssaameyliananda23@gmail.com>
2026-06-12 16:15:36 +0700

    Update KursiDAO.java

--- src/cityrailgo/dao/KursiDAO.java
+++ src/cityrailgo/dao/KursiDAO.java
@@ -1,5 +1,33 @@
 package cityrailgo.dao;
 
+import cityrailgo.model.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class KursiDAO {
-    
+
+    private List<Kursi> daftarKursi = new ArrayList<>();
+
+    public Kursi findById(int id) {
+
+        for (Kursi kursi : daftarKursi) {
+            if (kursi.getId() == id) {
+                return kursi;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Kursi> findByJadwal(Jadwal jadwal) {
+        return daftarKursi;
+    }
+
+    public List<Kursi> findTersediaByJadwal(Jadwal jadwal) {
+        return jadwal.getKursiTersedia();
+    }
+
+    public boolean update(Kursi kursi) {
+        return true;
+    }
 }
