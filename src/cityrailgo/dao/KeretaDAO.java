8d567ddedd86f2e5391fb88530eaba11dbafc4c8
rayssaameyliananda23-collab <rayssaameyliananda23@gmail.com>
2026-06-12 16:16:50 +0700

    Update KeretaDAO.java

--- src/cityrailgo/dao/KeretaDAO.java
+++ src/cityrailgo/dao/KeretaDAO.java
@@ -1,5 +1,44 @@
 package cityrailgo.dao;
 
+import cityrailgo.model.Kereta;
+import java.util.ArrayList;
+import java.util.List;
+
 public class KeretaDAO {
-    
+
+    private List<Kereta> daftarKereta = new ArrayList<>();
+
+    public Kereta findById(int id) {
+
+        for (Kereta kereta : daftarKereta) {
+            if (kereta.getId() == id) {
+                return kereta;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Kereta> findAll() {
+        return daftarKereta;
+    }
+
+    public boolean save(Kereta kereta) {
+        return daftarKereta.add(kereta);
+    }
+
+    public boolean update(Kereta kereta) {
+        return true;
+    }
+
+    public boolean delete(int id) {
+
+        Kereta kereta = findById(id);
+
+        if (kereta != null) {
+            return daftarKereta.remove(kereta);
+        }
+
+        return false;
+    }
 }
