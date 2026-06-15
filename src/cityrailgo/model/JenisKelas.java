f58b8f0fb86be8a157108a7f87577e385174748f
Diimsx <dimasml476@gmail.com>
2026-06-15 11:05:13 +0700

    Refactor JenisKelas class for price calculation
    Refactor JenisKelas class to remove unused fields and methods, and update constructor and pricing calculation.

--- src/cityrailgo/model/JenisKelas.java
+++ src/cityrailgo/model/JenisKelas.java
@@ -1,66 +1,43 @@
-package cityrailgo.model;
+package model;
 
 public class JenisKelas {
+
     private int id;
     private String namaKelas;
-    private String deskripsi;
-    private double multiplierHarga;
+    private double hargaPerKm;
+    private String fasilitas;
 
-    public JenisKelas() {}
-
-    public JenisKelas(int id, String namaKelas, String deskripsi, double multiplierHarga) {
-        this.id              = id;
-        this.namaKelas       = namaKelas;
-        this.deskripsi       = deskripsi;
-        this.multiplierHarga = multiplierHarga;
-    }
-
-    public double hitungHarga(double hargaBase) {
-        return hargaBase * multiplierHarga;
-    }
-
-    public double getMultiplier() {
-        return multiplierHarga;
+    public JenisKelas(String namaKelas, double hargaPerKm, String fasilitas) {
+        this.namaKelas = namaKelas;
+        this.hargaPerKm = hargaPerKm;
+        this.fasilitas = fasilitas;
     }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getNamaKelas() {
         return namaKelas;
     }
 
-    public void setNamaKelas(String namaKelas) {
-        this.namaKelas = namaKelas;
+    public double getHargaPerKm() {
+        return hargaPerKm;
     }
 
-    public String getDeskripsi() {
-        return deskripsi;
+    public String getFasilitas() {
+        return fasilitas;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setHargaPerKm(double hargaPerKm) {
+        this.hargaPerKm = hargaPerKm;
     }
 
-    public void setDeskripsi(String deskripsi) {
-        this.deskripsi = deskripsi;
-    }
-
-    public double getMultiplierHarga() {
-        return multiplierHarga;
-    }
-
-    public void setMultiplierHarga(double multiplierHarga) {
-        this.multiplierHarga = multiplierHarga;
-    }
-
-    @Override
-    public String toString() {
-        return "JenisKelas{" +
-                "id=" + id +
-                ", namaKelas='" + namaKelas + '\'' +
-                ", multiplierHarga=" + multiplierHarga +
-                '}';
+    public double hitungHarga(double jarakKm) {
+        return hargaPerKm * jarakKm;
     }
 }
