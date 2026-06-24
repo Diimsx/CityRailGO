2c6586eb91f02b0269f559315980d16b9e37fc9d
Diimsx <dimasml476@gmail.com>
2026-06-24 18:26:50 +0700

    add

--- src/util/SceneManager.java
+++ src/util/SceneManager.java
@@ -36,7 +36,7 @@ public class SceneManager {
     public static <T> T switchSceneAndGetController(String fxmlPath) {
         try {
             FXMLLoader loader = new FXMLLoader(
-                SceneManager.class.getResource("/view" + fxmlPath)
+                SceneManager.class.getResource("/view/" + fxmlPath)
             );
             Parent root = loader.load();
             Scene scene = new Scene(root);
